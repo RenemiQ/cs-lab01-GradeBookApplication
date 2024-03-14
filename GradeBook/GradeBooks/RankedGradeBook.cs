4be256f64f68bb4873e86328708ff9e07c98e821
RenemiQ <gabriel.talaga@microsoft.wsei.edu.pl>
2024-03-14 23:26:01 +0100

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -13,5 +13,30 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires 5 students");
+            }
+            int threshold = (int)Math.Ceiling(Students.Count * 0.2); 
+            var grades = Students.Select(s => s.AverageGrade).OrderByDescending(g => g).ToList();
+
+            if (grades.IndexOf(averageGrade) < threshold)
+                return 'A';
+            else if (grades.IndexOf(averageGrade) < threshold * 2)
+                return 'B';
+            else if (grades.IndexOf(averageGrade) < threshold * 3)
+                return 'C';
+            else if (grades.IndexOf(averageGrade) < threshold * 4)
+                return 'D';
+            else
+                return 'F';
+
+
+        }
     }
+   
+
 }
